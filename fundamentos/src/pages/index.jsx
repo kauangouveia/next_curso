ca9a2cf36c85e7630484719847c9e9c474125a6e
kauangouveia <kauangouveia64@gmail>
2022-02-09 16:02:52 -0300

    iniciado curso de next

---
+++ fundamentos/src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function home() {
+  return (
+    <div>
+      <h1>fundamentos next</h1>
+      <h1>framework</h1>
+      <p>{JSON.stringify({ nome: "joao", idade: 22 })}</p>
+    </div>
+  );
+}
